a310ee0cd455335e747e49a0758659f979995871
13hamza <hazmawwe786@gmail.com>
2024-01-21 12:59:12 +0500

    update fales

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from"./CourseInput.modules.css";
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -34,12 +35,12 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid && "invalid"}`}>
+      <div className={'${styles["form-control"]}${!isValid &&"styles.invalid"} '}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} onBlur={focusGoalHandler} />
         {!isValid && !isGoalFocus && <p className="error_msg">Enter Something</p>}
       </div> 
-           <div className={`form-control ${!isValid && "invalid"}`}>
+           <div className={styles["form-control"]}>
         <label>Phone Number</label>
         <input type="tel" onChange={phoneNumberChangeHandler} />
         {!isValid && !isGoalFocus && <p className="error_msg">Enter Phone Number</p>}
